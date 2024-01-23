5f18da0f1c07ce5071e226569d7cb475405403bb
Samuel <salvadomo@hotmail.com>
2024-01-23 13:48:00 -0700

    association index models

---
+++ models/index.js
@@ -0,0 +1,27 @@
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
+
+User.hasMany(Post, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'postId'
+});
+
+module.exports = { User, Post, Comment };
