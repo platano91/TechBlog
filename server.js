b6d2bdd1ec6c20787e478286a03c54458a1ff891
Samuel <salvadomo@hotmail.com>
2024-01-24 10:48:36 -0700

    session middleware

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sequelize = require('./config/config.js');
+const sequelize = require('./config/config');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +11,24 @@ app.use(express.urlencoded({ extended: true }));
 // Static directory
 app.use(express.static('public'));
 
+// Session middleware
+const sess = {
+    secret: process.env.SESSION_SECRET, // Make sure to add SESSION_SECRET in your .env file
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  
+  app.use(session(sess));
+
 // Routes
+// Note: You will need to require your route files at the top of this script
+// const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
+
 // app.use('/api', apiRoutes);
 // app.use('/', htmlRoutes);
 
