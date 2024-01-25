4b687c3497b2a19e2600cab8df09d5a127b8e2a3
Samuel <salvadomo@hotmail.com>
2024-01-24 20:58:41 -0700

    homeroutes control

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,1 +1,31 @@
-// controllers/homeRoutes.js
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Route to render the homepage
+router.get('/', async (req, res) => {
+    try {
+        // Get all posts and JOIN with user data
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', { 
+            posts, 
+            logged_in: req.session.logged_in 
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
